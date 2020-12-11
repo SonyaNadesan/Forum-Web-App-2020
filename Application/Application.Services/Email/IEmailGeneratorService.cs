0e82611c5284b67e2bf4e42b3d1dfc55ff9e5c28
Sonya <sonya@redford-avenue.co.uk>
2020-12-11 12:08:03 +0000

    email services

---
+++ Application/Application.Services/Email/IEmailGeneratorService.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Mail;
+
+namespace Application.Services.Email
+{
+    public interface IEmailGeneratorService
+    {
+        MailMessage CreateEmail(string toEmail, string fromEmail, string subject, string body);
+    }
+}
---
+++ Application/Application.Services/ServiceResponse.cs
@@ -0,0 +1,36 @@
+﻿namespace Application.Services
+{
+    public class ServiceResponse<TResult>
+    {
+        private string error;
+
+        public TResult Result { get; set; }
+
+        public string ErrorMessage
+        {
+            get
+            {
+                return error;
+            }
+
+            set
+            {
+                error = value;
+                IsValid = false;
+            }
+        }
+
+        public bool IsValid { get; set; }
+
+        public ServiceResponse()
+        {
+            IsValid = true;
+        }
+
+        public ServiceResponse(TResult entityData)
+        {
+            Result = entityData;
+            IsValid = true;
+        }
+    }
+}

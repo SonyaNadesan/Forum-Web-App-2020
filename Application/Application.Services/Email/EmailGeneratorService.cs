060245180d111ad827a7e8f93b70eada6fb1a9fc
Sonya <sonya@redford-avenue.co.uk>
2020-12-11 12:08:03 +0000

    email services and service response

---
+++ Application/Application.Services/Email/EmailGeneratorService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Mail;
+
+namespace Application.Services.Email
+{
+    public class EmailGeneratorService : IEmailGeneratorService
+    {
+        public MailMessage CreateEmail(string toEmail, string fromEmail, string subject, string body)
+        {
+            var to = new MailAddress(toEmail);
+            var from = new MailAddress(fromEmail);
+
+            var mail = new MailMessage(from, to);
+            mail.Subject = subject;
+            mail.Body = body;
+
+            return mail;
+        }
+    }
+}

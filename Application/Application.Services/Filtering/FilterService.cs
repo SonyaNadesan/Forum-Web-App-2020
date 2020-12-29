09341e6e4fbc593f6d168717299280ced609f2c5
Sonya <sonya@redford-avenue.co.uk>
2020-12-29 22:02:25 +0000

    filtering services

---
+++ Application/Application.Services/Filtering/FilterService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Application.Services.Filtering
+{
+    public class FilterService<T> : IFilterService<T>
+    {
+        public List<T> GetFilteredList(List<T> originalList, List<IFilter<T>> filters)
+        {
+            for (int i = 0; i < originalList.Count; i++)
+            {
+                foreach (var filter in filters)
+                {
+                    if (!filter.IsValid(originalList[i]))
+                    {
+                        originalList.RemoveAt(i);
+                        i = i - 1;
+                        break;
+                    }
+                }
+            }
+
+            return originalList;
+        }
+
+        public bool IsValidAgainstAllFilters(T item, List<IFilter<T>> filters)
+        {
+            foreach (var filter in filters)
+            {
+                if (!filter.IsValid(item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Application/Application.Services/Filtering/IFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Services.Filtering
+{
+    public interface IFilter<T>
+    {
+        string Description { get; }
+
+        bool IsValid(T item);
+    }
+}

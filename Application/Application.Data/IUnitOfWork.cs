04f6995fa4289c47d15c191944226f73844067e6
Sonya <sonya@redford-avenue.co.uk>
2020-12-29 14:59:29 +0000

    unit of work pattern implemented to ensure concurrency

---
+++ Application/Application.Data/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Application.Data.Repositories;
+
+namespace Application.Data
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+
+        IThreadRepository ThreadRepository { get; }
+
+        IPostRepository PostRepository { get; }
+
+        IReactionRepository ReactionRepository { get; }
+
+        void Save();
+    }
+}
---
+++ Application/Application.Data/UnitOfWork.cs
@@ -0,0 +1,66 @@
+﻿using Application.Data.Repositories;
+
+namespace Application.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _context;
+
+        private IUserRepository _userRepository;
+        private IThreadRepository _threadRepository;
+        private IPostRepository _postRepository;
+        private IReactionRepository _reactionRepository;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                _userRepository ??= new UserRepository(_context);
+
+                return _userRepository;
+            }
+        }
+
+        public IThreadRepository ThreadRepository
+        {
+            get
+            {
+                _threadRepository ??= new ThreadRepository(_context);
+
+                return _threadRepository;
+            }
+        }
+
+        public IPostRepository PostRepository
+        {
+            get
+            {
+
+                _postRepository ??= new PostRepository(_context);
+
+                return _postRepository;
+            }
+        }
+
+        public IReactionRepository ReactionRepository
+        {
+            get
+            {
+                _reactionRepository ??= new ReactionRepository(_context);
+
+                return _reactionRepository;
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}

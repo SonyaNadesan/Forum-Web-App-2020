7d43b5040be9f3985aad9ea891f9bb4ddf47becb
Sonya <sonya@redford-avenue.co.uk>
2021-01-09 19:56:46 +0000

    restored databases, disabled ssl

--- Application/Application.Data/Migrations/20210109193918_Initial.Designer.cs
+++ Application/Application.Data/Migrations/20210109193918_Initial.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Application.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20201229230229_AddFiedToUser")]
-    partial class AddFiedToUser
+    [Migration("20210109193918_Initial")]
+    partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {

a11c13f772703aa3401df0cae33519ca28633760
Sonya <sonya@redford-avenue.co.uk>
2021-01-20 14:05:34 +0000

    removed dead code

--- Application/Application.Web/ViewModels/PaginationWithId.cs
+++ Application/Application.Web/ViewModels/PaginationWithId.cs
@@ -10,13 +10,6 @@ namespace Application.Web.ViewModels
 
         public PaginationWithId(IEnumerable<T> allResults, int page, int pageSeize, int startPage, string formAction, string query = "") : base(allResults, page, pageSeize, startPage, formAction, query = "")
         {
-            ItemsToDisplay = allResults.Skip((page - 1) * pageSeize).Take(pageSeize).ToList();
-            CurrentPage = page;
-            PageSize = pageSeize;
-            TotalNumberOfResults = allResults.Count();
-            Query = query;
-            FormAction = formAction;
-            StartPage = startPage;
         }
     }
 }

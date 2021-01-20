db70a1b39f5d90c5b93b468f94e4074c380c871f
Sonya <sonya@redford-avenue.co.uk>
2021-01-20 09:25:26 +0000

    remved dead code

--- Application/Application.Web/Controllers/ForumController.cs
+++ Application/Application.Web/Controllers/ForumController.cs
@@ -3,7 +3,6 @@ using Application.Services.Forum;
 using Application.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Application.Web.Controllers
